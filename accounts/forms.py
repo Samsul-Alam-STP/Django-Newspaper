56c45e84536d85d3e162286bcabed902cb909b34
Saifurrahmanemon <saifemon789@gmail.com>
2021-09-27 05:50:17 -0700

    this is a the first commit

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.db.models.base import Model
+from django.forms import fields
+from .models import CustomUser
+
+
+class CustomUserCreationForm(usercrea)
