0bb79b4286cb11d78f547f4d08077e1bd21db975
Saifurrahmanemon <saifemon789@gmail.com>
2021-09-28 05:31:09 -0700

    added boostrap

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import resolve_url
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
