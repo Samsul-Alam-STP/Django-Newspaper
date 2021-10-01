0afcf45f27b025d1ac97c3254de123b2b3bcea0a
Saifurrahmanemon <saifemon789@gmail.com>
2021-10-01 02:38:00 -0700

    added fully functional authorizatin

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import request
 from django.urls import reverse_lazy
 from django.urls.base import reverse
 from django.views.generic import ListView, DetailView
@@ -11,12 +13,12 @@ class ArticleListView(ListView):
     template_name = "article_list.html"
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Articles
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Articles
     fields = (
         "title",
@@ -24,18 +26,29 @@ class ArticleUpdateView(UpdateView):
     )
     template_name = "article_edit.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Articles
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Articles
     template_name = "article_new.html"
     fields = (
         "title",
-        "author",
         "body",
     )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
